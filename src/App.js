d161bc4580be9c17ee38295b935d89929179516d
kassius <tedi_kass@hotmail.com>
2021-05-07 01:58:44 +0300

    functionality complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import {useState} from 'react';
 import './App.css';
+import people from './data'
+import Person from './Person'
+
 
 function App() {
+  const [currentPersonId, setCurrentPersonId] = useState(1)
+
+  const handleToRight = ()=>{
+    if (people.length === currentPersonId){
+      setCurrentPersonId(1)
+    }
+    else{
+      setCurrentPersonId((oldValue) => oldValue + 1)
+    }
+  }
+  const handleToLeft = ()=>{
+    if (1 === currentPersonId){
+      setCurrentPersonId(people.length)
+    }
+    else{
+      setCurrentPersonId((oldValue) => oldValue - 1)
+    }
+  }
+  const handleRandom = ()=>{
+     let randomNumber = Math.floor((Math.random() * (people.length-1) + 1))
+    if (randomNumber >= currentPersonId) randomNumber++
+      setCurrentPersonId(randomNumber)
+  }
+
+
+  let person = people.find((person) => person.id === currentPersonId)
   return (
+    <Person person={person} onLeft={handleToLeft} onRight={handleToRight} onRandom={handleRandom}/>
   );
 }
 
